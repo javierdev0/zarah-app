da453a404e3a2d783b2b176760c2b624665251be
Javier <javier.developer0@gmail.com>
2023-07-21 21:38:37 +0200

    feat(Root): input component and tests

---
+++ src/components/InputSearch.tsx
@@ -0,0 +1,13 @@
+import { InputHTMLAttributes } from 'react'
+
+export default function InputSearch(props: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <label>
+      <input
+        {...props}
+        type='search'
+        className='rounded-md border-2 py-2 pl-2 text-sm w-full placeholder:font-medium focus'
+      />
+    </label>
+  )
+}
---
+++ src/components/__tests__/InputSearch.test.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render } from '@testing-library/react'
+import InputSearch from '../InputSearch'
+
+describe('Component: InputSearch', () => {
+  const props = {
+    placeholder: 'Filter podcasts...',
+    type: 'number',
+    onChange: jest.fn()
+  }
+
+  let inputSearch: HTMLInputElement
+
+  beforeEach(() => {
+    const { getByPlaceholderText } = render(<InputSearch {...props} />)
+    inputSearch = getByPlaceholderText(props.placeholder) as HTMLInputElement
+  })
+
+  it('should render the InputSearch component', () => {
+    expect(inputSearch).toBeInTheDocument()
+  })
+
+  it('should have correct placeholder', () => {
+    expect(inputSearch).toHaveAttribute('placeholder', props.placeholder)
+  })
+
+  it('should always be a search type', () => {
+    expect(inputSearch).toHaveAttribute('type', 'search')
+  })
+
+  it('should render with empty value', () => {
+    expect(inputSearch).toHaveValue('')
+  })
+
+  it('should update value when input value change', () => {
+    const value = 'Testing value'
+    fireEvent.change(inputSearch, { target: { value } })
+    expect(inputSearch).toHaveValue(value)
+  })
+})
