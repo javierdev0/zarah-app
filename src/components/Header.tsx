e9f787bd6b554fea03ad4d2ba5e765ffbef0c6f8
Javier <javier.developer0@gmail.com>
2023-07-22 13:23:58 +0200

    feat(Root): add loader to the header for displaying content loading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Ping } from '@uiball/loaders'
+
 export default function Header() {
   return (
-    <header className='border-b py-2'>
+    <header className='border-b py-2 flex justify-between items-center sticky top-0 bg-white z-50'>
       <h1 className='font-bold text-cyan-600 text-lg'>Podcaster</h1>
+      <Ping size={45} speed={2} color='#1c95b7' />
     </header>
   )
 }
