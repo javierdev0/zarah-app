838195d562eb7f3b763d8750abac43508acfdafb
Javier <javier.developer0@gmail.com>
2023-07-25 01:59:53 +0200

    test(Root): add new test to Header component

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,19 +1,33 @@
 import { render } from '@testing-library/react'
 import Header from '../Header'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('Component: Header', () => {
   let component: ReturnType<typeof render>
   beforeEach(() => {
-    component = render(<Header />)
+    component = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    )
   })
 
   it('should render the Header component', () => {
     expect(component).toBeTruthy()
   })
 
-  it('should render the title', async () => {
-    const title = 'Podcaster'
-    const heading = component.getByRole('heading', { name: title })
-    expect(heading.textContent).toBe(title)
+  it('should render the link with the correct text', () => {
+    const link = component.getByRole('link')
+    expect(link.textContent).toBe('Podcaster')
+  })
+
+  it('should render the link', () => {
+    const link = component.getByRole('link')
+    expect(link).toBeTruthy()
+  })
+
+  it('should render the link with the correct href', () => {
+    const link = component.getByRole('link')
+    expect(link.getAttribute('href')).toBe('/')
   })
 })
