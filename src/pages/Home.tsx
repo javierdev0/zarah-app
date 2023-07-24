43f780037dd97e6b0210f499f4cdfac302f533c2
Javier <javier.developer0@gmail.com>
2023-07-25 01:58:53 +0200

    refactor(Root): add component for complete the view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import Header from '../components/Header'
+import { filterPodcastsByKeyword } from '../utils/filterPodcastsByKeyword.utils'
+import usePodcasts from '../hooks/usePodcasts'
+import InputSearch from '../components/InputSearch'
+import { useState } from 'react'
+import PodcastsList from '../components/PodcastsList'
+import CounterPodcasts from '../components/CounterPodcasts'
+import useTransition from '../hooks/useTransition'
 
 export default function Home() {
+  const [keyword, setKeyword] = useState<string>('')
+  const { podcasts, isLoading } = usePodcasts()
+  useTransition({ isLoading })
+
+  const podcastsFiltered = filterPodcastsByKeyword({ podcasts, keyword })
+
   return (
     <>
-      <Header />
+      <main className='w-[1000px]'>
+        <div className='flex items-center justify-end gap-3 px-4 mt-5'>
+          <CounterPodcasts quantity={podcastsFiltered.length} />
+
+          <InputSearch
+            placeholder='Filter podcasts..'
+            value={keyword}
+            onChange={event => setKeyword(event.target.value)}
+          />
+        </div>
+      </main>
+      <PodcastsList podcastsFiltered={podcastsFiltered} />
     </>
   )
 }
