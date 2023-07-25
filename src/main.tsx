a14da2208265e01bf4ca8c790fa3b3668c808169
Javier <javier.developer0@gmail.com>
2023-07-25 02:18:53 +0200

    feat(Root): add global provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.tsx'
+import TransitionContext from './contexts/Transition.context.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <TransitionContext.Provider>
+    <App />
+  </TransitionContext.Provider>
+)
