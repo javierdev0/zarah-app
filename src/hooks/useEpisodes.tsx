94368abdf10e7a8c96d0260163b325f3730ad5a2
Javier <javier.developer0@gmail.com>
2023-07-25 02:16:55 +0200

    feat(Root): add custom hook for handle episode data fetch

---
+++ src/hooks/useEpisodes.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import useLocalStorage from './useLocalStorage'
+import { isTimeExpired } from '../utils/timeExpired.utils'
+import { EpisodeResponseTransformed } from '../types/episode.type'
+import { getEpisodesFromAPI } from '../services/episode.service'
+
+type Params = {
+  podcastId: string
+}
+
+const useEpisodes = ({ podcastId }: Params) => {
+  const initialValue: EpisodeResponseTransformed = {
+    resultCount: 0,
+    results: []
+  }
+  const [episodes, setEpisodes] = useState<EpisodeResponseTransformed>(initialValue)
+  const [loading, setLoading] = useState(false)
+  const { valueStored, setLocalStorage, expiration } = useLocalStorage<EpisodeResponseTransformed>(
+    `episode ${podcastId || ''}`,
+    initialValue
+  )
+
+  const getEpisodes = async () => {
+    const isExpired = isTimeExpired({ time: expiration })
+    if (isExpired) {
+      setLoading(true)
+      const response = await getEpisodesFromAPI({ podcastId })
+      setEpisodes(response)
+      setLocalStorage(response)
+      setLoading(false)
+      return
+    }
+
+    setEpisodes(valueStored)
+  }
+
+  useEffect(() => {
+    getEpisodes()
+  }, [valueStored])
+
+  return { episodes: episodes.results, resultCount: episodes.resultCount, isLoading: loading }
+}
+
+export default useEpisodes
