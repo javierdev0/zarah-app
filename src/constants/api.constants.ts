c1fe14ab3a9e677bd06ebd2cd9ae3a677186e88f
Javier <javier.developer0@gmail.com>
2023-07-25 02:09:14 +0200

    feat(Root): add episode service and test

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,1 +1,3 @@
 export const API_PODCASTS = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
+export const API_EPISODES = 'https://itunes.apple.com/lookup'
+export const API_CORS = 'https://api.allorigins.win/get?url='
---
+++ src/services/__tests__/episode.service.test.ts
@@ -0,0 +1,69 @@
+import { MockEpisodeFormTransform } from '../../transformers/__mock__/episode.transformer.mock'
+import { EpisodeServiceMock, ResponseServiceTransformed } from '../__mocks__/episode.service.mock'
+import { getEpisodesFromAPI } from '../episode.service'
+import * as episodeTransformer from '../../transformers/episode.transformer'
+
+describe('Service: getEpisodes', () => {
+  const podcastId = '1635211340'
+  it('should call function with correct api', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ feed: { entry: EpisodeServiceMock } })
+    })
+    await getEpisodesFromAPI({ podcastId })
+    expect(global.fetch).toHaveBeenCalledWith(
+      `https://api.allorigins.win/get?url=https%3A%2F%2Fitunes.apple.com%2Flookup%3Fid%3D${podcastId}%26entity%3DpodcastEpisode%26media%3Dpodcast`
+    )
+  })
+
+  it('should return { results: [], resultCount: 0 } if api fails', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.reject({ feed: { entry: EpisodeServiceMock } })
+    })
+    const episodes = await getEpisodesFromAPI({ podcastId })
+    expect(episodes).toEqual({ results: [], resultCount: 0 })
+  })
+
+  it('should return { results: [], resultCount: 0 } if api returns empty array', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ feed: { entry: [] } })
+    })
+    const episodes = await getEpisodesFromAPI({ podcastId })
+    expect(episodes).toEqual({ results: [], resultCount: 0 })
+  })
+
+  it('should return { results: [], resultCount: 0 } if api returns undefined', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ feed: { entry: undefined } })
+    })
+    const episodes = await getEpisodesFromAPI({ podcastId })
+    expect(episodes).toEqual({ results: [], resultCount: 0 })
+  })
+
+  it('should get episodes from api (mock true)', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ contents: JSON.stringify({ resultCount: 2, results: MockEpisodeFormTransform }) })
+    })
+    const episodes = await getEpisodesFromAPI({ podcastId })
+
+    expect(episodes).toEqual(ResponseServiceTransformed)
+  })
+  //
+  it('should not get episodes from api (mock false)', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ contents: JSON.stringify({ resultCount: 2, results: {} }) })
+    })
+
+    const episodes = await getEpisodesFromAPI({ podcastId })
+    expect(episodes).not.toEqual(ResponseServiceTransformed)
+  })
+
+  it('should call transformEpisode with correct params', async () => {
+    const transformEpisodeMock = jest.spyOn(episodeTransformer, 'transformEpisode')
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ contents: JSON.stringify({ resultCount: 2, results: MockEpisodeFormTransform }) })
+    })
+    await getEpisodesFromAPI({ podcastId })
+    expect(transformEpisodeMock).toHaveBeenCalledWith({ episodes: MockEpisodeFormTransform })
+  })
+})
