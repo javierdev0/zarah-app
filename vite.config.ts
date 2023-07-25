9f465ebf8e1b48d6ec9f8b4c2ef15dd4cf9ff105
Javier <javier.developer0@gmail.com>
2023-07-25 02:31:03 +0200

    feat(Root): add developments mode dev and prod, minify and concat

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+
+  return {
+    plugins: [react()],
+    build: {
+      minify: isDev ? false : true,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+          inlineDynamicImports: true,
+          entryFileNames: '[name].js',
+          assetFileNames: '[name].[ext]'
+        }
+      }
+    }
+  }
 })
