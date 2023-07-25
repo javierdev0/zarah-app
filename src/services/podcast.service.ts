5c55e8ad386b503f2ebe1eea8097eb59b614904e
Javier <javier.developer0@gmail.com>
2023-07-25 02:02:14 +0200

    refactor(Root): add data transformer to podcast service

--- src/services/podcast.service.ts
+++ src/services/podcast.service.ts
@@ -1,12 +1,14 @@
 import { API_PODCASTS } from '../constants/api.constants'
+import { transformPodcast } from '../transformers/podcast.transformer'
 import { Podcast } from '../types/podcast.type'
 
 export const getPodcastsFromAPI = async (): Promise<Podcast[]> => {
   try {
     const response = await fetch(API_PODCASTS)
     const json = await response.json()
-    const podcasts = json.feed.entry
-    return podcasts
+    const podcasts: Podcast[] = json.feed.entry
+    const podcastsTransformed = transformPodcast({ podcasts })
+    return podcastsTransformed
   } catch (error) {
     console.error('Error getting podcasts:', error)
     return []
