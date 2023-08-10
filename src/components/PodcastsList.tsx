7f48b25075a734fc76ac8bdca5875df329c9c323
Javier <javier.developer0@gmail.com>
2023-08-10 13:01:41 +0200

    refactor(Root): remove unnecessary data from api response

--- src/components/PodcastsList.tsx
+++ src/components/PodcastsList.tsx
@@ -1,21 +1,16 @@
-import { Podcast } from '../types/podcast.type'
+import { PodcastResponseTransformed } from '../types/podcast.type'
 import CardPodcast from './CardPodcast'
 
 type Props = {
-  podcastsFiltered: Podcast[]
+  podcastsFiltered: PodcastResponseTransformed[]
 }
 
 export default function PodcastsList({ podcastsFiltered }: Props) {
   return (
     <ul className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 my-10 gap-y-40 items-start place-items-center'>
-      {podcastsFiltered.map((podcast: Podcast) => (
-        <li className='w-fit' key={podcast.id.attributes['im:id']}>
-          <CardPodcast
-            img={podcast['im:image'][2].label}
-            title={podcast['im:name'].label}
-            author={podcast['im:artist'].label}
-            podcastId={podcast.id.attributes['im:id']}
-          />
+      {podcastsFiltered.map((podcast: PodcastResponseTransformed) => (
+        <li className='w-fit' key={podcast.id}>
+          <CardPodcast img={podcast.image} title={podcast.title} author={podcast.artist} podcastId={podcast.id} />
         </li>
       ))}
     </ul>
--- src/transformers/podcast.transformer.ts
+++ src/transformers/podcast.transformer.ts
@@ -1,17 +1,18 @@
-import { Podcast } from '../types/podcast.type'
+import { Podcast } from '../modules/podcasts/domain/Podcast.domain'
+import { PodcastResponseTransformed } from '../types/podcast.type'
 
 type Params = {
   podcasts: Podcast[] | undefined
 }
 
-export const transformPodcast = ({ podcasts }: Params): Podcast[] => {
-  if (!podcasts) return []
-  const newPodcasts: Podcast[] = podcasts.map(entry => ({
-    id: entry.id,
-    'im:name': entry['im:name'],
-    'im:image': entry['im:image'],
-    'im:artist': entry['im:artist'],
-    summary: entry.summary
+export const transformPodcast = ({ podcasts }: Params): PodcastResponseTransformed[] => {
+  if (!podcasts || !Array.isArray(podcasts)) return []
+  const newPodcasts: PodcastResponseTransformed[] = podcasts.map(entry => ({
+    id: entry.id?.attributes['im:id'],
+    title: entry['im:name']?.label,
+    image: entry['im:image']?.[2]?.label,
+    artist: entry['im:artist']?.label,
+    summary: entry.summary?.label
   }))
 
   return newPodcasts
